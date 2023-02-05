c9fa9e0fa2a9053a0ac37bba3475b961e2bc81d4
wangjunchen <wangjunchen2017@outlook.com>
2023-02-05 19:01:48 +0800

    feat 基于SpringCloud实现服务提供者功能； 基于SpringCloud创建消费者并实现功能

---
+++ SpringCloud_004_goods_provider_eureka_client/src/main/java/com/powernode/controller/GoodsController.java
@@ -0,0 +1,26 @@
+package com.powernode.controller;
+
+import com.powernode.entity.Goods;
+import com.powernode.service.GoodsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wangjunchen
+ */
+@RestController
+@Slf4j
+public class GoodsController {
+    @Resource
+    private GoodsService goodsService;
+
+    @GetMapping("/goods/{id}")
+    public Goods getById(@PathVariable("id") Integer goodsId) {
+        log.info("进入商品微服务！！！");
+        return goodsService.getById(goodsId);
+    }
+}
--- SpringCloud_004_order_consumer_eureka_client/src/main/java/com/powernode/SpringCloud004OrderConsumerEurekaClientApplication.java
+++ SpringCloud_004_order_consumer_eureka_client/src/main/java/com/powernode/SpringCloud004OrderConsumerEurekaClientApplication.java
@@ -4,12 +4,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangjunchen
+ */
 @SpringBootApplication
 @MapperScan("com.powernode.dao")
-public class SpringCloud004GoodsProviderEurekaClientApplication {
+public class SpringCloud004OrderConsumerEurekaClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCloud004GoodsProviderEurekaClientApplication.class, args);
+        SpringApplication.run(SpringCloud004OrderConsumerEurekaClientApplication.class, args);
     }
 
 }
